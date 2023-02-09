d3f55c78937bf989697a5327e9ce493e1507dfb5
mmcheng <mmcheng@Hillstonenet.com>
2023-02-09 15:32:31 +0800

    update role

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,3 +4,8 @@ from flask import Blueprint
 main = Blueprint('main', __name__)  # 两个参数，一个是蓝本的名称， 一个是蓝本所在的包或模块
 
 from . import views, errors
+from ..models import Permission
+
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 # 用户模型
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
 from . import login_manager
 from itsdangerous import TimedSerializer as Serializer
 from authlib.jose import jwt, JoseError
@@ -14,13 +14,73 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+class Permission:
+    FOLLOW = 1
+    COMMENT = 2
+    WRITE = 4
+    MODERATE = 8
+    ADMIN = 16
+
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+
+# 此属性告诉Flask-Login使用应用自定义的匿名用户类
+login_manager.anonymous_user = AnonymousUser
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')  # users属性代表这个关系的面向对象视角，对于一个Role类的实例，
     # 其users属性将返回与角色相关联的用户组成的列表，第一个参数表明这个关系的另一端是哪个模型，backref参数向User模型中添加一个role属性，
     # 从而定义反向关系。通过User实例的这个属性可以获取对应的Role模型对象
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permission(self):
+        self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm     # &位与运算，== 返回true或者false
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE],
+            'Moderator': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE, Permission.MODERATE],
+            'Administrator': [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE, Permission.MODERATE, Permission.ADMIN]
+        }
+        default_role = 'User'
+        for r in roles:  # 取得角色名User,Moderator,Administrator
+            role = Role.query.filter_by(name=r).first()  # 查询数据库中是否有角色r
+            if role is None:
+                role = Role(name=r)
+            role.reset_permission()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)   # 是否是默认角色User, True of False
+            db.session.add(role)
+        db.session.commit()
+
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -31,13 +91,25 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
     # 密码散列值
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(64), unique=True, index=True)
-
     # 账户确认状态位
     confirmed = db.Column(db.Boolean, default=False)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if self.role is None:        # 如果创建基类对象后还没有定义角色，则根据电子邮件地址决定是否将其设为管理员或默认角色
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(name='Administrator').first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
+
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
 
     @property
     def password(self):
